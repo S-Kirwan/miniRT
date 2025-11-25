e9369420b76750748a59d8322d64c3f422c04f1d
Sean Kirwan <skirwan@student.42london.com>
2025-11-25 14:05:22 +0000

    Defined structs
    Defined structs for each element to be used in the scene, subject to
    adjustment

--- inc/miniRT.h
+++ inc/miniRT.h
@@ -6,7 +6,7 @@
 /*   By: skirwan <skirwan@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 11:44:15 by skirwan           #+#    #+#             */
-/*   Updated: 2025/11/25 11:51:05 by skirwan          ###   ########.fr       */
+/*   Updated: 2025/11/25 13:06:08 by skirwan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,91 @@
 # define MINIRT_H
 
 # include <unistd.h>
+# include <fcntl.h>
+# include <sys/stat.h>
 # include "libft.h"
 
+typedef struct s_data
+{
+	struct s_ambience	*ambience;
+	struct s_camera		*camera;
+	struct s_light		*lights;
+	struct s_sphere		*spheres;
+	struct s_cylinder	*cylinders;
+	struct s_plane		*planes;
+}	t_data;
+
+// Ratio for the ambience is input from 0 to 1 in the input file, but we
+// multiply this by 100 so that we can use an int and not a float
+// This may not be helpful at all, subject to change - I just thought ints
+// are easier to work with compared to floats where possible
+typedef struct s_ambience
+{
+	int	ratio;
+	int	red;
+	int	green;
+	int	blue;
+}	t_ambience;
+
+typedef struct s_camera
+{
+	float	x;
+	float	y;
+	float	z;
+	float	x_orientation;
+	float	y_orientation;
+	float	z_orientation;
+	int		fov;
+}	t_camera;
+
+typedef struct s_light
+{
+	float	x;
+	float	y;
+	float	z;
+	int		brightness;
+	int		red;
+	int		green;
+	int		blue;
+}	t_light;
+
+typedef struct s_sphere
+{
+	float	x;
+	float	y;
+	float	z;
+	float	diameter;
+	int		red;
+	int		green;
+	int		blue;
+}	t_sphere;
+
+typedef struct s_plane
+{
+	float	x;
+	float	y;
+	float	z;
+	float	x_vector;
+	float	y_vector;
+	float	z_vector;
+	int		red;
+	int		green;
+	int		blue;
+}	t_plane;
+
+typedef struct s_cylinder
+{
+	float	x;
+	float	y;
+	float	z;
+	float	x_vector;
+	float	y_vector;
+	float	z_vector;
+	float	diameter;
+	float	height;
+	int		red;
+	int		green;
+	int		blue;
+}	t_cylinder;
 
 #endif
--- inc/parsing.h
+++ inc/parsing.h
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parsing.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skirwan <skirwan@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/25 11:23:16 by skirwan           #+#    #+#             */
-/*   Updated: 2025/11/25 13:12:13 by skirwan          ###   ########.fr       */
+/*   Created: 2025/11/25 11:43:28 by skirwan           #+#    #+#             */
+/*   Updated: 2025/11/25 13:06:09 by skirwan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "miniRT.h"
-#include "parsing.h"
+#ifndef PARSING_H
+# define PARSING_H
 
-int	main(int argc, char **argv)
+# include "miniRT.h"
+
+typedef struct s_parser
 {
-	int	scene_fd;
+	int	ambience;
+	int	camera;
+	int	light;
+	int	spheres;
+	int	planes;
+	int	cylinders;
+}	t_parser;
 
-	if ((scene_fd = validate_file(argc, argv)) == -1)
-		return (1);
-	return (0);
-}
+int	validate_file(int argc, char **argv);
+
+#endif
+
