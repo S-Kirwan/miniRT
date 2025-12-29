b3ecccb5bb4e778423f7fcf9613ddf990e92b95b
aramos-gua <alejandro.ramos.gua@gmail.com>
2025-12-29 18:45:53 +0000

    jamon

---
+++ build/screen.d
@@ -0,0 +1,23 @@
+build/screen.o: src/screen.c src/../includes/minirt.h \
+  includes/../libft/libft.h libft/./ft_printf/ft_printf.h \
+  libft/./ft_printf/../libft.h includes/../minilibx-linux/mlx.h \
+  src/../includes/miniRT.h libft/libft.h src/../includes/parsing.h \
+  includes/miniRT.h
+
+src/../includes/minirt.h:
+
+includes/../libft/libft.h:
+
+libft/./ft_printf/ft_printf.h:
+
+libft/./ft_printf/../libft.h:
+
+includes/../minilibx-linux/mlx.h:
+
+src/../includes/miniRT.h:
+
+libft/libft.h:
+
+src/../includes/parsing.h:
+
+includes/miniRT.h:
