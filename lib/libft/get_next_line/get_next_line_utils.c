0b6b2c89d379c0302b793d96ee41bb57edf85b44
aramos-gua <alejandro.ramos.gua@gmail.com>
2026-02-12 14:52:49 +0000

    added missing files in libft: atof, isspace and add_back.c, had to uncomment the t_list struct in libft.h for it. Also, commented the ft_strlen inside get_next_line. Compiling. nothing showing on the screen yet. Need to connect code of screen.c with parsing variables

--- lib/libft/get_next_line/get_next_line_utils.c
+++ lib/libft/get_next_line/get_next_line_utils.c
@@ -54,12 +54,12 @@ int	ft_final_len(t_list *lst)
 	return (size);
 }
 
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
+// int	ft_strlen(char *str)
+// {
+// 	int	i;
+//
+// 	i = 0;
+// 	while (str[i] != '\0')
+// 		i++;
+// 	return (i);
+// }
